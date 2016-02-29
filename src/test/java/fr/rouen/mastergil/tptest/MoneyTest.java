37ca1faf8d61b0fed80930d74613eb6c71cbfad9
Rudy Baer <baer.rudy@gmail.com>
2016-02-29 21:14:42 +0100

    TP3

--- src/test/java/fr/rouen/mastergil/tptest/MoneyTest.java
+++ src/test/java/fr/rouen/mastergil/tptest/MoneyTest.java
@@ -7,49 +7,81 @@ package fr.rouen.mastergil.tptest;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MoneyTest {
 
     @Test
     public void constructeurVideDoitRemplirMontantEtDevise() {
+        //Given
+
+        //When
         Money money = new Money();
 
-        assertEquals(0, money.getMontant());
-        assertEquals(Devise.EURO, money.getDevise());
+        // then
+        assertThat(money.getMontant()).isEqualTo(0);
+        assertThat(money.getDevise()).isEqualTo(Devise.EURO);
     }
 
     @Test
     public void constructeurDoitRemplirMontantEtDevise() {
+        //Given
+
+        //When
         Money money = new Money(5, Devise.EURO);
 
-        assertEquals(5, money.getMontant());
-        assertEquals(Devise.EURO, money.getDevise());
+        //Then
+        assertThat(money.getMontant()).isEqualTo(5);
+        assertThat(money.getDevise()).isEqualTo(Devise.EURO);
     }
 
     @Test
     public void constructeurInterditDeviseInconnueOuVide() {
+        //Given
+        final Money money = new Money(20, Devise.EURO);
         try {
-            new Money(5, null);
-            fail("Une devise null n'est pas autorisée");
+            //When
+            money.setDevise(null);
+            //Then
+            fail("setDevise doit envoyer une exception si aucune devise n'est spécifiée");
         } catch (IllegalArgumentException iae) {
-            ; // comportement normal
+            //Then
+            assertEquals("Devise doit être spécifiée", iae.getMessage());
         }
     }
 
+
     @Test
-    public void isPositifDoitRetournerFalseSiLeMontantEstNegatif() {
-        Money money = new Money();
-        money.setMontant(-2);
+    public void isPositif_doit_etre_vrai_si_montant_superieur_a_zero() throws Exception {
+        //Given
+        final Money money = new Money(20, Devise.EURO);
+        //When
         final boolean isPositif = money.isPositif();
-        assertFalse(isPositif);
+        //Then
+        assertThat(isPositif).isTrue();
     }
 
     @Test
-    public void isPositifDoitRetournerTrueSiLeMontantEstPositif() {
-        Money money = new Money();
-        money.setMontant(2);
+    public void isPositif_doit_etre_vrai_si_montant_egal_a_zero() throws Exception {
+        //Given
+        final Money money = new Money(0, Devise.EURO);
+        //When
         final boolean isPositif = money.isPositif();
-        assertTrue(isPositif);
+        //Then
+        assertThat(isPositif).isTrue();
     }
+
+    @Test
+    public void isPositif_doit_etre_faux_si_montant_inferieur_a_zero() throws Exception {
+        //Given
+        final Money money = new Money(-20, Devise.EURO);
+        //When
+        final boolean isPositif = money.isPositif();
+        //Then
+        assertThat(isPositif).isFalse();
+    }
+
+
 
 
 }
